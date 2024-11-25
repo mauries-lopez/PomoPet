0be7464571419d2dbf97a754bf62614c2046d689
koko-tsuu <colleen_tan@dlsu.edu.ph>
2024-11-25 10:18:13 +0800

    UI edits to builder_exercise_video_desc to be a scrollview

--- Machine Project Development/app/src/main/java/com/example/pomopet/ExerciseDataSet.kt
+++ Machine Project Development/app/src/main/java/com/example/pomopet/ExerciseDataSet.kt
@@ -16,7 +16,7 @@ class ExerciseDataSet {
                         "and raise them above your head. Make sure to do all of these things simultaneously. \n\nAfter landing on " +
                         "the ground, reverse the pattern and return to your starting position with arms by your side and feet " +
                         "together. \n\nRepeat the entire process, performing between 10 to 100 reps for about six sets. Remember " +
-                        "to maintain your posture and avoid slouching or twisting your toes outward.\n\n"))
+                        "to maintain your posture and avoid slouching or twisting your toes outward.\n"))
             data.add(ViewExerDataModel("Squats", R.drawable.squats,"<iframe width=\"100%\" " +
                     "height=\"100%\" src=\"https://www.youtube.com/embed/4Pg2PeH9U_Y?si=dH4lw3a_tv2Ht1dH\" " +
                     "title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; " +
@@ -28,7 +28,7 @@ class ExerciseDataSet {
                     "as you push your hips back. \n\nLower yourself into a squat until either your heels begin to lift " +
                     "off the floor, or until your torso begins to round or flex forward. Your depth should be " +
                     "determined by your form. \n\nKeep your chest out and core tight as you push through your heels to " +
-                    "stand back up to your starting position. Squeeze your glutes at the top."))
+                    "stand back up to your starting position. Squeeze your glutes at the top.\n"))
             data.add(ViewExerDataModel("Lunges", R.drawable.lunges,"<iframe width=\"100%\" height=\"100%\" " +
                     "src=\"https://www.youtube.com/embed/1QS-kExwLY4?si=HpdsVtpjIs9Y1fgN\" title=\"YouTube video player\" " +
                     "frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; " +
@@ -38,7 +38,7 @@ class ExerciseDataSet {
                     "one leg is ahead of your torso and the other is behind. Your foot should land flat and remain " +
                     "flat while it’s on the ground. Your rear heel will rise off of the ground. \n\nBend your knees to " +
                     "approximately 90 degrees as you lower yourself. Remember to keep your trunk upright and core " +
-                    "engaged. Then, forcefully push off from your front leg to return to the starting position."))
+                    "engaged. Then, forcefully push off from your front leg to return to the starting position.\n"))
             return data
         }
     }
